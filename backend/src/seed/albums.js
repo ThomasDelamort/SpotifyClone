0d3a2b7cfb6652bd57e3e6d6180dc8e7c689ab38
Chris Paredes <nealparedes776@gmail.com>
2026-06-08 23:46:13 +0800

    deleted

--- backend/src/seed/albums.js
+++ backend/src/seed/albums.js
@@ -6,85 +6,141 @@ import { config } from "dotenv";
 config();
 
 const seedDatabase = async () => {
-    try {
-        await mongoose.connect(process.env.MONGODB_URI);
+	try {
+		await mongoose.connect(process.env.MONGODB_URI);
 
-        // Clear existing data
-        // await Album.deleteMany({});
-        // await Song.deleteMany({});
+		// Clear existing data
+		// await Album.deleteMany({});
+		// await Song.deleteMany({});
 
-        // First, create all songs
-        const createdSongs = await Song.insertMany([
-            {
-                title: "Smells Like Teen Spirit",
-                artist: "Nirvana",
-                imageUrl: "/albums/Nevermind.jpg",
-                audioUrl: "/songs/SmellsLikeTeenSpirit.mp3",
-                plays: Math.floor(Math.random() * 5000),
-                duration: 303,
-            },
-            {
-                title: "In Bloom",
-                artist: "Nirvana",
-                imageUrl: "/albums/Nevermind.jpg",
-                audioUrl: "/songs/InBloom.mp3",
-                plays: Math.floor(Math.random() * 5000),
-                duration: 255,
-            },
-            {
-                title: "Come As You Are",
-                artist: "Nirvana",
-                imageUrl: "/albums/Nevermind.jpg",
-                audioUrl: "/songs/ComeAsYouAre.mp3",
-                plays: Math.floor(Math.random() * 5000),
-                duration: 220,
-            },
-            {
-                title: "Breed",
-                artist: "Nirvana",
-                imageUrl: "/albums/Nevermind.jpg",
-                audioUrl: "/songs/Breed.mp3",
-                plays: Math.floor(Math.random() * 5000),
-                duration: 184,
-            },
-            {
-                title: "Lithium",
-                artist: "Nirvana",
-                imageUrl: "/albums/Nevermind.jpg",
-                audioUrl: "/songs/Lithium.mp3",
-                plays: Math.floor(Math.random() * 5000),
-                duration: 258,
-            },
-        ]);
+		// First, create all songs
+		const createdSongs = await Song.insertMany([
+			{
+				title: "Cave In",
+				artist: "Owl City",
+				imageUrl: "https://res.cloudinary.com/dzaxbhml4/image/upload/v1780924547/ab67616d0000b273785d4e702802da500fc78b32_q7usx6.jpg",
+				audioUrl: "https://res.cloudinary.com/dzaxbhml4/video/upload/v1780924719/01_-_Cave_In_Album_Version_slflqu.mp3",
+				plays: Math.floor(Math.random() * 5000),
+				duration: 242,
+			},
+			{
+				title: "The Bird and the Worm",
+				artist: "Owl City",
+				imageUrl: "https://res.cloudinary.com/dzaxbhml4/image/upload/v1780924547/ab67616d0000b273785d4e702802da500fc78b32_q7usx6.jpg",
+				audioUrl: "https://res.cloudinary.com/dzaxbhml4/video/upload/v1780924760/02_-_The_Bird_And_The_Worm_i2wtlz.mp3",
+				plays: Math.floor(Math.random() * 5000),
+				duration: 207,
+			},
+			{
+				title: "Hello Seattle",
+				artist: "Owl City",
+				imageUrl: "https://res.cloudinary.com/dzaxbhml4/image/upload/v1780924547/ab67616d0000b273785d4e702802da500fc78b32_q7usx6.jpg",
+				audioUrl: "https://res.cloudinary.com/dzaxbhml4/video/upload/v1780924764/03_-_Hello_Seattle_asdmew.mp3",
+				plays: Math.floor(Math.random() * 5000),
+				duration: 167,
+			},
+			{
+				title: "Umbrella Beach",
+				artist: "Owl City",
+				imageUrl: "https://res.cloudinary.com/dzaxbhml4/image/upload/v1780924547/ab67616d0000b273785d4e702802da500fc78b32_q7usx6.jpg",
+				audioUrl: "https://res.cloudinary.com/dzaxbhml4/video/upload/v1780924773/04_-_Umbrella_Beach_Album_Version_ca5plg.mp3",
+				plays: Math.floor(Math.random() * 5000),
+				duration: 230,
+			},
+			{
+				title: "The Saltwater Room",
+				artist: "Owl City",
+				imageUrl: "https://res.cloudinary.com/dzaxbhml4/image/upload/v1780924547/ab67616d0000b273785d4e702802da500fc78b32_q7usx6.jpg",
+				audioUrl: "https://res.cloudinary.com/dzaxbhml4/video/upload/v1780924808/05_-_The_Saltwater_Room_wyzwkd.mp3",
+				plays: Math.floor(Math.random() * 5000),
+				duration: 242,
+			},
+			{
+				title: "Dental Care",
+				artist: "Owl City",
+				imageUrl: "https://res.cloudinary.com/dzaxbhml4/image/upload/v1780924547/ab67616d0000b273785d4e702802da500fc78b32_q7usx6.jpg",
+				audioUrl: "https://res.cloudinary.com/dzaxbhml4/video/upload/v1780924825/06_-_Dental_Care_Album_Version_yarcx2.mp3",
+				plays: Math.floor(Math.random() * 5000),
+				duration: 191,
+			},
+			{
+				title: "Meteor Shower",
+				artist: "Owl City",
+				imageUrl: "https://res.cloudinary.com/dzaxbhml4/image/upload/v1780924547/ab67616d0000b273785d4e702802da500fc78b32_q7usx6.jpg",
+				audioUrl: "https://res.cloudinary.com/dzaxbhml4/video/upload/v1780924811/07_-_Meteor_Shower_Album_Version_mblqwu.mp3",
+				plays: Math.floor(Math.random() * 5000),
+				duration: 134,
+			},
+			{
+				title: "On the Wing",
+				artist: "Owl City",
+				imageUrl: "https://res.cloudinary.com/dzaxbhml4/image/upload/v1780924547/ab67616d0000b273785d4e702802da500fc78b32_q7usx6.jpg",
+				audioUrl: "https://res.cloudinary.com/dzaxbhml4/video/upload/v1780924850/08_-_On_The_Wing_i3pizo.mp3",
+				plays: Math.floor(Math.random() * 5000),
+				duration: 301,
+			},
+			{
+				title: "Fireflies",
+				artist: "Owl City",
+				imageUrl: "https://res.cloudinary.com/dzaxbhml4/image/upload/v1780924547/ab67616d0000b273785d4e702802da500fc78b32_q7usx6.jpg",
+				audioUrl: "https://res.cloudinary.com/dzaxbhml4/video/upload/v1780924782/09_-_Fireflies_uwkfyv.mp3",
+				plays: Math.floor(Math.random() * 5000),
+				duration: 228,
+			},
+			{
+				title: "The Tip of the Iceberg",
+				artist: "Owl City",
+				imageUrl: "https://res.cloudinary.com/dzaxbhml4/image/upload/v1780924547/ab67616d0000b273785d4e702802da500fc78b32_q7usx6.jpg",
+				audioUrl: "https://res.cloudinary.com/dzaxbhml4/video/upload/v1780924741/10_-_The_Tip_Of_The_Iceberg_Album_Version_nvinzz.mp3",
+				plays: Math.floor(Math.random() * 5000),
+				duration: 203,
+			},
+			{
+				title: "Vanilla Twilight",
+				artist: "Owl City",
+				imageUrl: "https://res.cloudinary.com/dzaxbhml4/image/upload/v1780924547/ab67616d0000b273785d4e702802da500fc78b32_q7usx6.jpg",
+				audioUrl: "https://res.cloudinary.com/dzaxbhml4/video/upload/v1780924853/11_-_Vanilla_Twilight_lsw9md.mp3",
+				plays: Math.floor(Math.random() * 5000),
+				duration: 232,
+			},
+			{
+				title: "Tidal Wave",
+				artist: "Owl City",
+				imageUrl: "https://res.cloudinary.com/dzaxbhml4/image/upload/v1780924547/ab67616d0000b273785d4e702802da500fc78b32_q7usx6.jpg",
+				audioUrl: "https://res.cloudinary.com/dzaxbhml4/video/upload/v1780924851/12_-_Tidal_Wave_Album_Version_esijjf.mp3",
+				plays: Math.floor(Math.random() * 5000),
+				duration: 190,
+			},
+		]);
 
-        // Create albums with references to song IDs
-        const albums = [
-            {
-                title: "Nevermind",
-                artist: "Nirvana",
-                imageUrl: "/albums/Nevermind.jpg",
-                releaseYear: 1991,
-                songs: createdSongs.slice(0, 4).map((song) => song._id),
-            },
-        ];
+		// Create albums with references to song IDs
+		const albums = [
+			{
+				title: "Ocean Eyes",
+				artist: "Owl City",
+				imageUrl: "https://res.cloudinary.com/dzaxbhml4/image/upload/v1780924547/ab67616d0000b273785d4e702802da500fc78b32_q7usx6.jpg",
+				releaseYear: 2009,
+				songs: createdSongs.map((song) => song._id),
+			},
+		];
 
-        // Insert all albums
-        const createdAlbums = await Album.insertMany(albums);
+		// Insert all albums
+		const createdAlbums = await Album.insertMany(albums);
 
-        // Update songs with their album references
-        for (let i = 0; i < createdAlbums.length; i++) {
-            const album = createdAlbums[i];
-            const albumSongs = albums[i].songs;
+		// Update songs with their album references
+		for (let i = 0; i < createdAlbums.length; i++) {
+			const album = createdAlbums[i];
+			const albumSongs = albums[i].songs;
 
-            await Song.updateMany({ _id: { $in: albumSongs } }, { albumId: album._id });
-        }
+			await Song.updateMany({ _id: { $in: albumSongs } }, { albumId: album._id });
+		}
 
-        console.log("Database seeded successfully!");
-    } catch (error) {
-        console.error("Error seeding database:", error);
-    } finally {
-        mongoose.connection.close();
-    }
+		console.log("Database seeded successfully!");
+	} catch (error) {
+		console.error("Error seeding database:", error);
+	} finally {
+		mongoose.connection.close();
+	}
 };
 
 seedDatabase();
