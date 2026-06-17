51311854e721bf1c94d0ff0e11b6b7bbc98fbd52
Chris Paredes <nealparedes776@gmail.com>
2026-06-17 12:27:27 +0800

    Adding bonus track to albums

--- frontend/src/pages/album/AlbumPage.tsx
+++ frontend/src/pages/album/AlbumPage.tsx
@@ -59,7 +59,7 @@ const AlbumPage = () => {
                             <p className="text-sm font-medium">
                                 Album
                             </p>
-                            <h1 className='text-7xl font-bold my-4'>{currentAlbum?.title}</h1>
+                            <h1 className='text-6xl font-bold my-4'>{currentAlbum?.title}</h1>
                             <div className="flex items-center gap-2 text-sm text-zinc-100">
                                 <span className="font-medium text-white">{currentAlbum?.artist}</span>
                                 <span>• {currentAlbum?.songs.length} songs</span>
