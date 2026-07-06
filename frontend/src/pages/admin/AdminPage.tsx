8dbc2795f6bc10367074080a7be4e484e8309028
Christian Neal <nealparedes776@gmail.com>
2026-07-06 20:33:55 +0800

    Prettier format

--- frontend/src/pages/admin/AdminPage.tsx
+++ frontend/src/pages/admin/AdminPage.tsx
@@ -1,58 +1,62 @@
-import {useAuthStore} from "@/stores/useAuthStore";
+import { useAuthStore } from "@/stores/useAuthStore";
 import Header from "@/pages/admin/components/Header";
 import DashboardStats from "@/pages/admin/components/DashboardStats";
-import {Tabs, TabsContent, TabsList, TabsTrigger} from "@/components/ui/tabs";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import { Album, Music } from "lucide-react";
 import SongsTabContent from "@/pages/admin/components/SongsTabContent";
 import AlbumsTabContent from "@/pages/admin/components/AlbumsTabContent";
-import {useEffect} from "react";
+import { useEffect } from "react";
 import { useMusicStore } from "@/stores/useMusicStore";
 
-
 const AdminPage = () => {
+  const { isAdmin, isLoading } = useAuthStore();
 
-    const { isAdmin, isLoading } = useAuthStore()
+  const { fetchAlbums, fetchSongs, fetchStats } = useMusicStore();
 
-    const { fetchAlbums, fetchSongs, fetchStats } = useMusicStore();
+  useEffect(() => {
+    fetchAlbums();
+    fetchSongs();
+    fetchStats();
+  }, [fetchAlbums, fetchSongs, fetchStats]);
 
-    useEffect(() => {
-        fetchAlbums();
-        fetchSongs();
-        fetchStats();
-    }, [fetchAlbums, fetchSongs, fetchStats])
+  if (!isAdmin && !isLoading) return <div>Unauthorized</div>;
 
-    if(!isAdmin && !isLoading) return <div>Unauthorized</div>
+  return (
+    <div
+      className="min-h-screen bg-linear-to-b from-zinc-900 via-zinc-900
+        to-black text-zinc-100 p-8"
+    >
+      <Header />
 
+      <DashboardStats />
 
-    return (
-        <div className="min-h-screen bg-gradient-to-b from-zinc-900 via-zinc-900
-        to-black text-zinc-100 p-8">
+      <Tabs defaultValue="songs" className="space-y-6">
+        <TabsList className="p-1 bg-zinc-800/50">
+          <TabsTrigger
+            value="songs"
+            className="data-[state=active]:bg-zinc-700"
+          >
+            <Music className="mr-2 size-4" />
+            Songs
+          </TabsTrigger>
+          <TabsTrigger
+            value="albums"
+            className="data-[state=active]:bg-zinc-700"
+          >
+            <Album className="mr-2 size-4" />
+            Albums
+          </TabsTrigger>
+        </TabsList>
 
-        <Header />
+        <TabsContent value="songs">
+          <SongsTabContent />
+        </TabsContent>
 
-        <DashboardStats />
-
-        <Tabs defaultValue="songs" className="space-y-6">
-            <TabsList className="p-1 bg-zinc-800/50">
-                <TabsTrigger value="songs" className="data-[state=active]:bg-zinc-700">
-                    <Music className="mr-2 size-4" />
-                    Songs
-                </TabsTrigger>
-                <TabsTrigger value="albums" className="data-[state=active]:bg-zinc-700">
-                    <Album className="mr-2 size-4" />
-                    Albums
-                </TabsTrigger>
-            </TabsList>
-
-            <TabsContent value="songs">
-            <SongsTabContent />
-            </TabsContent>
-
-            <TabsContent value="albums">
-            <AlbumsTabContent />
-            </TabsContent>
-        </Tabs>
-        </div>
-    )
-}
-export default AdminPage
+        <TabsContent value="albums">
+          <AlbumsTabContent />
+        </TabsContent>
+      </Tabs>
+    </div>
+  );
+};
+export default AdminPage;
--- frontend/src/pages/admin/components/SongsTabContent.tsx
+++ frontend/src/pages/admin/components/SongsTabContent.tsx
@@ -1,27 +1,33 @@
-import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
 import { Music } from "lucide-react";
 import SongsTable from "@/pages/admin/components/SongsTable";
 import AddSongDialog from "@/pages/admin/components/AddSongDialog";
 
 const SongsTabContent = () => {
-    return (
-        <Card>
-            <CardHeader>
-                <div className="flex items-center justify-between">
-                    <div>
-                        <CardTitle className="flex items-center gap-2">
-                            <Music className="size-5 text-emerald-500" />
-                            Songs Library
-                        </CardTitle>
-                        <CardDescription>Manage your music Tracks</CardDescription>
-                    </div>
-                    <AddSongDialog />
-                </div>
-            </CardHeader>
-            <CardContent>
-                <SongsTable />
-            </CardContent>
-        </Card>
-    )
-}
-export default SongsTabContent
+  return (
+    <Card>
+      <CardHeader>
+        <div className="flex items-center justify-between">
+          <div>
+            <CardTitle className="flex items-center gap-2">
+              <Music className="size-5 text-emerald-500" />
+              Songs Library
+            </CardTitle>
+            <CardDescription>Manage your music Tracks</CardDescription>
+          </div>
+          <AddSongDialog />
+        </div>
+      </CardHeader>
+      <CardContent>
+        <SongsTable />
+      </CardContent>
+    </Card>
+  );
+};
+export default SongsTabContent;
