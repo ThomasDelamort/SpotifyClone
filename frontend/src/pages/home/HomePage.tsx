e11dda0f83ab3dd7a1f4571bf59872f49c3bf79b
Christian Neal <nealparedes776@gmail.com>
2026-07-07 18:20:06 +0800

    gradient to linear

--- frontend/src/pages/home/HomePage.tsx
+++ frontend/src/pages/home/HomePage.tsx
@@ -7,85 +7,72 @@ import SectionGrid from "@/pages/home/components/SectionGrid.tsx";
 import { usePlayerStore } from "@/stores/usePlayerStore.ts";
 
 const HomePage = () => {
-    const {
-        fetchFeaturedSongs,
-        fetchMadeForYouSongs,
-        fetchTrendingSongs,
-        isLoading,
-        madeForYouSongs,
-        featuredSongs,
-        trendingSongs,
-    } = useMusicStore();
+  const {
+    fetchFeaturedSongs,
+    fetchMadeForYouSongs,
+    fetchTrendingSongs,
+    isLoading,
+    madeForYouSongs,
+    featuredSongs,
+    trendingSongs,
+  } = useMusicStore();
 
-    const { initializeQueue } = usePlayerStore();
+  const { initializeQueue } = usePlayerStore();
 
-    useEffect(() => {
-        fetchFeaturedSongs();
-        fetchMadeForYouSongs();
-        fetchTrendingSongs();
-    }, [
-        fetchFeaturedSongs,
-        fetchMadeForYouSongs,
-        fetchTrendingSongs,
-    ]);
+  useEffect(() => {
+    fetchFeaturedSongs();
+    fetchMadeForYouSongs();
+    fetchTrendingSongs();
+  }, [fetchFeaturedSongs, fetchMadeForYouSongs, fetchTrendingSongs]);
 
-    useEffect(() => {
-        if (
-            madeForYouSongs.length > 0 &&
-            featuredSongs.length > 0 &&
-            trendingSongs.length > 0
-        ) {
-            const allSongs = [
-                ...featuredSongs,
-                ...trendingSongs,
-                ...madeForYouSongs,
-            ];
+  useEffect(() => {
+    if (
+      madeForYouSongs.length > 0 &&
+      featuredSongs.length > 0 &&
+      trendingSongs.length > 0
+    ) {
+      const allSongs = [...featuredSongs, ...trendingSongs, ...madeForYouSongs];
 
-            initializeQueue(allSongs);
-        }
-    }, [
-        initializeQueue,
-        madeForYouSongs,
-        featuredSongs,
-        trendingSongs,
-    ]);
+      initializeQueue(allSongs);
+    }
+  }, [initializeQueue, madeForYouSongs, featuredSongs, trendingSongs]);
 
-    console.log({
-        isLoading,
-        madeForYouSongs,
-        featuredSongs,
-        trendingSongs,
-    });
+  console.log({
+    isLoading,
+    madeForYouSongs,
+    featuredSongs,
+    trendingSongs,
+  });
 
-    return (
-        <div className="rounded-md overflow-hidden h-full bg-gradient-to-b from-zinc-800 to-zinc-900">
-            <TopBar />
+  return (
+    <div className="rounded-md overflow-hidden h-full bg-linear-to-b from-zinc-800 to-zinc-900">
+      <TopBar />
 
-            <ScrollArea className="h-[calc(100vh-180px)]">
-                <div className="p-4 sm:p-6">
-                    <h1 className="text-2xl sm:text-3xl font-bold mb-6">
-                        Good Afternoon
-                    </h1>
+      <ScrollArea className="h-[calc(100vh-180px)]">
+        <div className="p-4 sm:p-6">
+          <h1 className="text-2xl sm:text-3xl font-bold mb-6">
+            Good Afternoon
+          </h1>
 
-                    <FeaturedSection />
+          <FeaturedSection />
 
-                    <div className="space-y-8">
-                        <SectionGrid
-                            title="Made For You"
-                            songs={madeForYouSongs}
-                            isLoading={isLoading}
-                        />
+          <div className="space-y-8">
+            <SectionGrid
+              title="Made For You"
+              songs={madeForYouSongs}
+              isLoading={isLoading}
+            />
 
-                        <SectionGrid
-                            title="Trending"
-                            songs={trendingSongs}
-                            isLoading={isLoading}
-                        />
-                    </div>
-                </div>
-            </ScrollArea>
+            <SectionGrid
+              title="Trending"
+              songs={trendingSongs}
+              isLoading={isLoading}
+            />
+          </div>
         </div>
-    );
+      </ScrollArea>
+    </div>
+  );
 };
 
 export default HomePage;
